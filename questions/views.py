8168912e32b3070bfb9b6e135d88dbf838106330
Luis Penagos <luis@penagos.co>
2020-06-17 18:32:16 -0400

    Update model typo

--- questions/views.py
+++ questions/views.py
@@ -4,7 +4,6 @@ from .models import Question
 
 # Create your views here.
 def index(request):
-    questions = Question.objects.order_by('-title')[:5]
+    questions = Question.objects.all
     context = {'questions': questions}
-    context = {}
     return render(request, 'questions/index.html', context)
