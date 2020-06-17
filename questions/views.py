bc1d6e2182d7c6193e5f5b149d13020e2463343e
Luis Penagos <luis@penagos.co>
2020-06-16 22:55:44 -0400

    Begin hooking up questions default view

--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 def index(request):
+    questions = Question.objects.order_by('-title')[:5]
+    context = {'questions': questions}
     context = {}
     return render(request, 'questions/index.html', context)
