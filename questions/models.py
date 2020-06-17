457de571d84958c58ad8291b0263ee78dfb5fd7a
Luis Penagos <luis@penagos.co>
2020-06-16 22:45:57 -0400

    Begin Question and Post models

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.title;
+ 
+class Post(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    body = models.TextField
