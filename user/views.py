cb8b54852128b182873706b97560157d0dc15d75
Luis Penagos <luis@penagos.co>
2020-06-19 18:13:58 -0400

    Begin laying groundwork for login form AJAX support

--- user/views.py
+++ user/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def login(request):
-    context = {}
-    return render(request, 'user/login.html', context)
+    # Handle both POST and GET requests
+    if request.method == "POST":
+        return JsonResponse({'login': True})
+    else:
+        context = {}
+        return render(request, 'user/login.html', context)
 
 def join(request):
     context = {}
