c3e3fdd7c8f05aeac49b436c201adaf728ecec87
Luis Penagos <luis@penagos.co>
2020-06-19 18:22:38 -0400

    Hook up login form to Django authentication backend

--- user/views.py
+++ user/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login(request):
     # Handle both POST and GET requests
     if request.method == "POST":
-        return JsonResponse({'login': True})
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            # Successful login
+            success = True
+        else:
+            # Failure
+            success = False
+
+        return JsonResponse({'login': success})
     else:
         context = {}
         return render(request, 'user/login.html', context)
