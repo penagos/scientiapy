161c40a666949103486617aacde41ef37dc99696
Luis Penagos <luis@penagos.co>
2020-08-24 17:44:57 -0400

    Begin adding sorting logic

--- questions/views.py
+++ questions/views.py
@@ -17,12 +17,23 @@ def index(request):
     # If a search was made, filter on needle
     title = 'Questions'
 
+    # Handle sorting of results if specified
+    sort = request.GET.get('sort', '')
+
+    if sort == 'new':
+        sort = '-published_date'
+    elif sort == 'hot':
+        sort = 'published_date'
+    else:
+        sort = '-post_type'
+
     if request.GET.get('q') is not None:
         # Search across tags, answers and questions
         # Puts questions before answer hits
         query = request.GET.get('q')
         title = 'Search Results for "{}"'.format(query)
-        questions = Post.objects.filter(Q(title__icontains=query) | Q(body__icontains=query) | Q(tags__icontains=query)).annotate(answers=Count('post')).order_by('-post_type')
+
+        questions = Post.objects.filter(Q(title__icontains=query) | Q(body__icontains=query) | Q(tags__icontains=query)).annotate(answers=Count('post')).order_by(sort)
 
         # Walk result set and prune accordingly
         questionsFiltered = []
@@ -42,7 +53,7 @@ def index(request):
         title = 'Questions tagged "{}"'.format(query)
         questions = Post.objects.filter(Q(tags__icontains=query)).annotate(answers=Count('post'))
     else:
-        questions = Post.objects.filter(post_type=PostType.QUESTION).annotate(answers=Count('post')).order_by('-id')
+        questions = Post.objects.filter(post_type=PostType.QUESTION).annotate(answers=Count('post')).order_by(sort)
 
     # Convert comma separated tags to list for easy display
     for post in questions:
