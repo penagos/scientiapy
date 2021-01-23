4c312610b4c21a09b30e004030d90a3ec83db385
Luis Penagos <luis@penagos.co>
2021-01-22 20:33:12 -0500

    Fix see all results link on typeahead dropdown

--- questions/views.py
+++ questions/views.py
@@ -109,6 +109,7 @@ def index(request):
             serialized['tags'] = post.tags
             serialized['url'] = reverse('questions:view', args=(serialized['qid'], ))
             serialized['author'] = post.author.username
+            serialized['author_url'] = reverse('user:profile', args=(post.author.id, ))
             serialized['published_date'] = post.published_date
 
             response.append(serialized)
